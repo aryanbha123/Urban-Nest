68451f35bcc584e32ff5b266b1e286416d77b92e
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-06-29 14:02:49 +0530

    gh

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Caursel from '../components/Caursel'
 import Invest from '../components/Invest'
+import Contact from '../components/Contact'
 export default function Home() {
   return (
     <div style={{marginTop:"105px"}}>
       <Hero/>
       <Caursel/>
       <Invest/>
+      <Contact/>
     </div>
   )
 }
