9f69b9f84ed3608086994f935346fcd9413b5474
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-06-29 13:25:54 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -13,7 +13,7 @@ export default function Hero() {
                     <div className="container relative mx-auto">
                         <div className="items-center flex flex-wrap">
                             <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-                                <div className="pr-12">
+                                <div className="">
                                     <h1 className="text-white font-semibold text-5xl">
                                         Welcome to Urban Nest.
                                     </h1>
