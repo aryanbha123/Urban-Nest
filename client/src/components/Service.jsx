dfa9eeb3e5031a325d7b7a65cd69737dc58b1c80
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-06-29 15:41:34 +0530

    Update Service.jsx

--- client/src/components/Service.jsx
+++ client/src/components/Service.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Service() {
     return (
-        <div>
+        <div id="services">
             <section className="bg-white pb-6">
                 <div className="max-w-7xl mx-auto p-4 sm:p-3 lg:p-1">
                     <div className="container mx-auto px-6  bg-white">
