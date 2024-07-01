2321a4394a9f589efab876c4b25cdf6905fbe712
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-07-01 17:02:28 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import { Suspense , lazy} from 'react';
+import { Suspense, lazy } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/footer';
 import Loader from './components/Loader';
 
+const Header = lazy(() => import('./components/Header'));
+const Footer = lazy(() => import('./components/Footer'));
 const Home = lazy(() => import('./pages/Home'));
 
 function App() {
   return (
     <>
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<Loader />}>
         <BrowserRouter>
-          <Header />
+          <Suspense fallback={<Loader />}>
+            <Header />
+          </Suspense>
           <Routes>
             <Route path='/' element={<Home />}></Route>
           </Routes>
+          <Suspense fallback={<Loader />}>
+            <Footer />
+          </Suspense>
         </BrowserRouter>
-        <Footer />
       </Suspense>
-
     </>
   );
 }
