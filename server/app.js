29b3d3bfa96b45dbaffb985e2b8a95134ff3af61
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-07-16 11:30:07 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 app.use(bodyParser.json());
 app.post('/api/contact', (req, res) => {
   const { name, email, phone, message } = req.body;
 
-  // Here you would typically handle the data (e.g., save it to a database or send an email)
+
   console.log('Received contact form data:', { name, email, phone, message });
 
   res.status(200).send({ message: 'Message received' });
